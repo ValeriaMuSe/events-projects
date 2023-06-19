db002b0f3d3cb816c1551271781bf84bdf37b649
Vale <valeriams13@gmail.com>
2023-06-19 15:44:32 -0600

    Buttons changes

--- js/modules/buttons.js
+++ js/modules/buttons.js
@@ -12,40 +12,22 @@ export { eventApi, formatDate, formatPrice };
 
 function renderEvents(eventsData) {
   var gridContainer = document.getElementById('grid-container');
-  
   // Limpiar la cuadrícula de eventos existente
-  if (gridContainer) {
-    gridContainer.innerHTML = '';
-  } else {
+  if (!gridContainer) {
     gridContainer = document.createElement('div');
     gridContainer.id = 'grid-container';
     document.body.appendChild(gridContainer);
   }
-  
-  eventsData.forEach(event => {
-    var eventElement = document.createElement('div');
-    eventElement.classList.add('event');
-
-    var imageElement = document.createElement('img');
-    imageElement.src = event.image;
-    imageElement.classList.add('event_image');
-    eventElement.appendChild(imageElement);
-
-    var titleElement = document.createElement('h2');
-    titleElement.textContent = event.title;
-    eventElement.appendChild(titleElement);
-
-    var dateElement = document.createElement('p');
-    dateElement.textContent = formatDate(event.date);
-    eventElement.appendChild(dateElement);
-
-
-    var priceElement = document.createElement('p');
-    priceElement.textContent = formatPrice(event.price);
-    eventElement.appendChild(priceElement);
-
-    gridContainer.appendChild(eventElement);
-  });
+  gridContainer.innerHTML = eventsData
+    .map((event) => `
+      <div class="event">
+        <img class="event_image" src="${event.image}">
+        <p class="event_title">${event.title}</p>
+        <p>${formatDate(event.date)}</p>
+        <p>${formatPrice(event.price)}</p>
+      </div>
+    `)
+    .join('');
 }
 
 function handleClick(category) {
@@ -63,27 +45,33 @@ function initializeButtons() {
   var container = document.createElement('div');
   container.id = 'tab-container';
   document.body.appendChild(container);
-
   var eventscategories = ['Music', 'Sports', 'Business', 'Food', 'Art'];
-
-  eventscategories.forEach(function(category) {
+  var defaultButton = null; // Variable para almacenar el botón predeterminado
+  eventscategories.forEach(function (category) {
     var button = document.createElement('button');
     button.innerHTML = category;
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
       console.log('Categoría seleccionada: ' + category);
+      if (defaultButton !== null) {
+        defaultButton.classList.remove('default-tab-button');
+      }
       handleClick(category.toLowerCase()); // Convertir a minúsculas para que coincida con las categorías en el API
+      button.classList.add('default-tab-button');
+      defaultButton = button; // Actualizar el botón predeterminado
     });
     container.appendChild(button);
+    if (category === 'Music') {
+      button.classList.add('default-tab-button');
+      defaultButton = button; // Establecer el botón de la categoría "Music" como predeterminado
+    }
   });
-
   container.classList.add('flex-container');
-
   var buttons = container.getElementsByTagName('button');
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].classList.add('tab-button');
   }
+  handleClick('music');
 }
-
 export { initializeButtons };
 
 
